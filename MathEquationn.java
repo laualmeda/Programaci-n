1f1783dd04e1fcf08709c4b85f5cee860b2cce6a
Ruiz-Ashly <168213970+Ruiz-Ashly@users.noreply.github.com>
2024-04-26 12:55:01 -0400

    Create MathEquationn.java
    Ashly primera parte del proyeto

---
+++ MathEquationn.java
@@ -0,0 +1,7 @@
+public class MathEquationn {
+    public static void main(String[] args) {
+        double x = 2.0; // Valor de la variable independiente
+        double y = calculateEquation(x);
+        System.out.println("El valor de y es: " + y);
+    }
+}
